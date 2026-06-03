d9340e072f9187c10a5d139cf4cb20903f0f4fc8
iblamedee <sirdee424@gmail.com>
2026-06-03 07:40:21 +0530

    vector database for rag

---
+++ langchain/check.py
@@ -0,0 +1,8 @@
+# from langchain_community.document_loaders import PyPDFLoader
+
+
+# file_path = r"X:\api_trail\langchain\LLMs @PROVIDERHUB0.pdf"
+# loader = PyPDFLoader(file_path)
+# doc = loader.load()
+
+# print(f"Total number of pages: {len(doc)}")
---
+++ langchain/main.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_qdrant import QdrantVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+
+file_path = r"X:\api_trail\langchain\LLMs @PROVIDERHUB0.pdf"
+loader = PyPDFLoader(file_path)
+doc = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+
+chunk_size = 800,
+chunk_overlap = 200
+
+)
+
+chunks = text_splitter.split_documents(documents=doc)
+
+# emdedding_model = GoogleGenerativeAIEmbeddings(
+#     model = "embedding-001",
+#     api_key = "AIzaSyBZFHqkzIBlIj0hbQ5D8Afx1-XYK18PiEQ"
+# )
+
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    openai_api_key= os.getenv("OPENROUTER_API_KEY"),
+    openai_api_base="https://openrouter.ai/api/v1"
+    
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents = chunks,
+    embedding= embedding_model,
+    url = "http://localhost:6333",
+    collection_name = "learning-rag4",
+    force_recreate=True
+
+)
+
+print("Vector store created successfully!")
